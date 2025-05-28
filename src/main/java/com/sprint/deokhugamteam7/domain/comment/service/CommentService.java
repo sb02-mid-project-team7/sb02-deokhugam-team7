3b2f452fa90b802c0f7138400dc69eab09af20ca
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-28 21:45:08 +0900

    feat: [댓글 등록] 기능 구현 완료

--- src/main/java/com/sprint/deokhugamteam7/domain/comment/service/CommentService.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/comment/service/CommentService.java
@@ -4,7 +4,12 @@ import com.sprint.deokhugamteam7.domain.comment.dto.CommentDto;
 import com.sprint.deokhugamteam7.domain.comment.dto.request.CommentCreateRequest;
 import com.sprint.deokhugamteam7.domain.comment.dto.request.CommentUpdateRequest;
 import com.sprint.deokhugamteam7.domain.comment.dto.response.CursorPageResponseCommentDto;
+import com.sprint.deokhugamteam7.domain.comment.entity.Comment;
 import com.sprint.deokhugamteam7.domain.comment.repository.CommentRepository;
+import com.sprint.deokhugamteam7.domain.review.entity.Review;
+import com.sprint.deokhugamteam7.domain.review.repository.ReviewRepository;
+import com.sprint.deokhugamteam7.domain.user.entity.User;
+import com.sprint.deokhugamteam7.domain.user.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -16,30 +21,55 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CommentService {
 
-  private final CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
 
-  public CommentDto create(CommentCreateRequest commentCreateRequest) {
-    log.debug("메시지 생성 시작: request={}", commentCreateRequest);
-    return null;
-  }
+    public CommentDto create(CommentCreateRequest commentCreateRequest) {
+        UUID userId = commentCreateRequest.userId();
+        UUID reviewId = commentCreateRequest.reviewId();
 
-  public CommentDto update(CommentUpdateRequest commentUpdateRequest) {
-    log.debug("메시지 수정 시작: request={}", commentUpdateRequest);
-    return null;
-  }
+        User user = userRepository.findById(userId).orElseThrow();
+        Review review = reviewRepository.findById(reviewId).orElseThrow();
 
-  public void deleteHard(UUID commentId, UUID userId) {
+        String content = commentCreateRequest.content();
 
-  }
+        Comment newComment = Comment.create(
+            user,
+            review,
+            content
+        );
 
-  public void deleteSoft(UUID commentId, UUID userId) {
+        Comment savedComment = commentRepository.save(newComment);
+        return new CommentDto(
+            savedComment.getId(),
+            savedComment.getReview().getId(),
+            savedComment.getUser().getId(),
+            savedComment.getUser().getNickname(),
+            savedComment.getContent(),
+            savedComment.getCreatedAt(),
+            savedComment.getUpdatedAt()
+        );
+    }
 
-  }
+    public CommentDto update(CommentUpdateRequest commentUpdateRequest) {
+        log.debug("메시지 수정 시작: request={}", commentUpdateRequest);
+        return null;
+    }
+
+    public void deleteHard(UUID commentId, UUID userId) {
+
+    }
+
+    public void deleteSoft(UUID commentId, UUID userId) {
+
+    }
 
 
-  public CursorPageResponseCommentDto getCommentList(UUID reviewId, String direction, String cursor,
-      LocalDateTime after, int limit) {
+    public CursorPageResponseCommentDto getCommentList(UUID reviewId, String direction,
+        String cursor,
+        LocalDateTime after, int limit) {
 
-    return null;
-  }
+        return null;
+    }
 }
