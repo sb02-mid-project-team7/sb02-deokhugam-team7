ff3ddfec3e62567d50c66646c4941e1f531cecf9
최규원 <rbdnjs1011@naver.com>
2025-05-29 11:40:51 +0900

    feat: 사용자 회원가입 구현

--- src/main/java/com/sprint/deokhugamteam7/domain/user/controller/UserController.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.sprint.deokhugamteam7.domain.user.controller;
 
+import com.sprint.deokhugamteam7.domain.user.dto.request.UserRegisterRequest;
+import com.sprint.deokhugamteam7.domain.user.dto.response.UserDto;
+import com.sprint.deokhugamteam7.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<UserDto> register(@Valid @RequestBody UserRegisterRequest request) {
+    UserDto user = userService.register(request);
+    return ResponseEntity.status(201).body(user);
+  }
 }
--- src/main/java/com/sprint/deokhugamteam7/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/service/impl/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.sprint.deokhugamteam7.domain.user.dto.request.UserLoginRequest;
 import com.sprint.deokhugamteam7.domain.user.dto.request.UserRegisterRequest;
 import com.sprint.deokhugamteam7.domain.user.dto.request.UserUpdateRequest;
 import com.sprint.deokhugamteam7.domain.user.dto.response.UserDto;
+import com.sprint.deokhugamteam7.domain.user.entity.User;
 import com.sprint.deokhugamteam7.domain.user.repository.UserRepository;
 import com.sprint.deokhugamteam7.domain.user.service.UserService;
+import com.sprint.deokhugamteam7.exception.DeokhugamException;
+import com.sprint.deokhugamteam7.exception.ErrorCode;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +20,24 @@ public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
 
   public UserDto register(UserRegisterRequest request) {
-    return null;
+    if (userRepository.existsByEmail(request.email())) {
+      throw new DeokhugamException(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    User user = User.create(
+        request.email(),
+        request.nickname(),
+        request.password()
+    );
+
+    User savedUser = userRepository.save(user);
+
+    return new UserDto(
+        savedUser.getId(),
+        savedUser.getEmail(),
+        savedUser.getNickname(),
+        savedUser.getCreateAt()
+    );
   }
 
   public UserDto login(UserLoginRequest request) {
