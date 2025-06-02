5855f40e2e40c216c2da01f6231afcd432f3454c
최규원 <rbdnjs1011@naver.com>
2025-06-02 14:45:45 +0900

    feat: 파워 유저 점수를 계산하는 스케쥴러 작성

--- src/main/java/com/sprint/deokhugamteam7/DeokhugamTeam7Application.java
+++ src/main/java/com/sprint/deokhugamteam7/DeokhugamTeam7Application.java
@@ -2,7 +2,9 @@ package com.sprint.deokhugamteam7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DeokhugamTeam7Application {
 
---
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/scheduler/PowerUserScoreSchedule.java
@@ -0,0 +1,30 @@
+package com.sprint.deokhugamteam7.domain.user.scheduler;
+
+import com.sprint.deokhugamteam7.constant.Period;
+import com.sprint.deokhugamteam7.domain.user.service.PowerUserService;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PowerUserScoreSchedule {
+  private final PowerUserService powerUserService;
+
+  @Scheduled(cron = "0 0 09 * * *")
+  public void calculatePowerUserScoresDaily() {
+    LocalDate today = LocalDate.now();
+    log.info("파워 유저 점수 계산 시작: {}", today);
+
+    for (Period period : Period.values()) {
+      log.info("기간: {}", period);
+      powerUserService.calculateAndSaveUserScores(period, today);
+      powerUserService.updateRanksForPeriodAndDate(period, today);
+    }
+
+    log.info("파워 유저 점수 계산 완료");
+  }
+}
