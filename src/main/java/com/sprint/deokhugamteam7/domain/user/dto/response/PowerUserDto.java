8ed5844685d29879ca6e7dc47e3d1d6a10a1a6e6
최규원 <rbdnjs1011@naver.com>
2025-06-02 14:39:07 +0900

    fix: PowerUserDto 수정

--- src/main/java/com/sprint/deokhugamteam7/domain/user/dto/response/PowerUserDto.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/dto/response/PowerUserDto.java
@@ -1,13 +1,14 @@
 package com.sprint.deokhugamteam7.domain.user.dto.response;
 
+import com.sprint.deokhugamteam7.constant.Period;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record PowerUserDto(
     UUID userId,
     String nickname,
-    String period,
-    LocalDateTime createAt,
+    Period period,
+    LocalDateTime createdAt,
     long rank,
     double score,
     double reviewScoreSum,
