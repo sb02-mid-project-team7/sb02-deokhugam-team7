f7d4e75152104b7c6fb31391ca9b4e51902e99a6
Seong-taeHan <kmljjn22@gmail.com>
2025-06-03 11:01:01 +0900

    feat: GlobalExceptionAdvice로직 구현

--- src/main/java/com/sprint/deokhugamteam7/exception/ErrorCode.java
+++ src/main/java/com/sprint/deokhugamteam7/exception/ErrorCode.java
@@ -2,13 +2,16 @@ package com.sprint.deokhugamteam7.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-  INTERNAL_SERVER_ERROR(500, "S001", "Internal Server Error");
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "Internal Server Error"),
+  NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "?", "알림을 찾을 수 없습니다."),
+  NOTIFICATION_NOT_OWNED(HttpStatus.FORBIDDEN, "?", "본인의 알람만 조회/수정할 수 있습니다.");
 
-  private final int httpStatus;
+  private final HttpStatus httpStatus;
   private final String code;
   private final String message;
 }
