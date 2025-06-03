fb60f10d9f51c141926c918a6f39f4e723a3a1ac
최규원 <rbdnjs1011@naver.com>
2025-06-03 13:25:16 +0900

    refactor: UserDTO 정적 팩토리 메서드 추가

--- src/main/java/com/sprint/deokhugamteam7/domain/user/dto/response/UserDto.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/dto/response/UserDto.java
@@ -1,5 +1,6 @@
 package com.sprint.deokhugamteam7.domain.user.dto.response;
 
+import com.sprint.deokhugamteam7.domain.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -9,5 +10,12 @@ public record UserDto(
     String nickname,
     LocalDateTime createdAt
 ) {
-
+  public static UserDto from(User user) {
+    return new UserDto(
+        user.getId(),
+        user.getEmail(),
+        user.getNickname(),
+        user.getCreatedAt()
+    );
+  }
 }
--- src/main/java/com/sprint/deokhugamteam7/domain/user/repository/UserRepository.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/user/repository/UserRepository.java
@@ -12,7 +12,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
   boolean existsByEmail(String email);
 
-  Optional<User> findByEmail(String email);
+  @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false ")
+  Optional<User> findByEmailIsDeletedFalse(String email);
 
   @Query("SELECT u FROM User u WHERE u.id = :id AND u.isDeleted = false")
   Optional<User> findByIdAndIsDeletedFalse(UUID id);
