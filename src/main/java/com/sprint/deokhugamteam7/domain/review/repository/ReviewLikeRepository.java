c08c2d1f7086f6d6e41dc336a164aed70282750b
seonhyelin <seonseon933@gmail.com>
2025-06-10 17:11:32 +0900

    Refactor: findAll, popular의 성능 저하 코드 개선.

--- src/main/java/com/sprint/deokhugamteam7/domain/review/repository/ReviewLikeRepository.java
+++ src/main/java/com/sprint/deokhugamteam7/domain/review/repository/ReviewLikeRepository.java
@@ -1,7 +1,9 @@
 package com.sprint.deokhugamteam7.domain.review.repository;
 
 import com.sprint.deokhugamteam7.domain.review.entity.ReviewLike;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +20,11 @@ public interface ReviewLikeRepository extends JpaRepository<ReviewLike, UUID> {
   boolean existsByUserIdAndReviewId(@Param("userId") UUID userId, @Param("reviewId") UUID reviewId);
 
   Optional<ReviewLike> findByReviewIdAndUserId(UUID reviewId, UUID userId);
+
+  @Query("SELECT rl.review.id "
+      + "FROM ReviewLike rl "
+      + "WHERE rl.user.id = :userId "
+      + "AND rl.review.id IN :reviewIds")
+  Set<UUID> findReviewIdsLikedByUser(@Param("userId") UUID userId,
+      @Param("reviewIds") List<UUID> reviewIds);
 }
